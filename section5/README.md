a8ac7fd5c6aeeaaa255257309fde54576b1bbc7c
g-lilian <50647023+g-lilian@users.noreply.github.com>
2022-11-23 23:07:27 +0800

    Add README to section5

---
+++ section5/README.md
@@ -0,0 +1,25 @@
+# Section 5: Machine Learning
+
+Dependencies:
+- scikit-learn
+- pandas
+
+Train:
+```
+python3 train.py
+```
+
+Train and predict buying price for input features ['high', '4', 'big', 'high', 'good']:
+```
+python3 predict.py
+```
+
+Output obtained from Decision Tree Classifier:
+```
+Train accuracy:
+Accuracy 0.3217535153019024, Precision 0.4708165322038361, Recall 0.3217535153019024
+Test accuracy:
+Accuracy 0.3159922928709056, Precision 0.4581909892549521, Recall 0.3159922928709056
+
+The predicted buying price is low.
+```
--- section5/train.py
+++ section5/train.py
@@ -1,8 +1,5 @@
 from utils import Utils
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import svm
 
 
 def train():
@@ -11,10 +8,7 @@ def train():
     dataset = utils.load_df(CARS_DATASET)
     X_train, X_test, y_train, y_test = utils.define_train_test_split(dataset)
 
-    # model = KNeighborsClassifier(n_neighbors=10)
-    # model = RandomForestClassifier(n_estimators=100)
-    # model = svm.SVC()
-    model = DecisionTreeClassifier(criterion='gini', max_depth=2, random_state=42)
+    model = DecisionTreeClassifier(max_depth=2, random_state=42)
     model.fit(X_train, y_train)
 
     print("Train accuracy:")
