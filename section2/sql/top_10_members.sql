483a6f0229d7cc4b3cdb7ad687137cb5148b84c8
g-lilian <50647023+g-lilian@users.noreply.github.com>
2022-11-23 21:20:48 +0800

    Fix bugs in section2 ER diagram and sql

--- section2/sql/top_10_members.sql
+++ section2/sql/top_10_members.sql
@@ -1,6 +1,7 @@
 -- Find the top 10 members by spending
-SELECT member_id, SUM(cost_dollars_per_qty * quantity_purchased) AS total_expenditure
-FROM Transactions
+SELECT member_id, SUM(cost_dollars * quantity_purchased) AS total_expenditure
+FROM Transactions LEFT JOIN Product
+  ON Transactions.product_id = Product.product_id
 GROUP BY member_id
 ORDER BY total_expenditure DESC
 LIMIT 10
