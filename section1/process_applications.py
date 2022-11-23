0ca68b192775de8a454331497c9b8a976084610b
g-lilian <50647023+g-lilian@users.noreply.github.com>
2022-11-23 17:15:31 +0800

    Update read_csv logic and add comments in process_applications

--- section1/process_applications.py
+++ section1/process_applications.py
@@ -1,8 +1,33 @@
+from datetime import datetime
+from glob import glob
 import os
 import sys
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+
+
+# Load CSVs in folder with latest date time into Spark DF
+# Assumptions: folders are named in %Y%m%d%H format and we always want to process the latest data
+def read_csvs(spark):
+    DT_FORMAT = "%Y%m%d%H"
+
+    latest_dt = datetime.strptime("2022112200", DT_FORMAT)
+    latest_dt_path = ""
+    for folder in glob("applications/*", recursive=False):
+        dt = datetime.strptime(folder.split("\\")[-1], DT_FORMAT)
+        if dt >= latest_dt:
+            latest_dt = dt
+            latest_dt_path = folder
+
+    df = None
+    for file in glob(latest_dt_path + "/*"):
+        if not df:
+            df = spark.read.csv(file, header=True, inferSchema=True)
+        else:
+            df.union(spark.read.csv(file, header=True, inferSchema=True))
+
+    return datetime.strftime(latest_dt, DT_FORMAT), df
 
 
 # Caveat: for some dates, it's impossible to distinguish between month and day
@@ -22,8 +47,9 @@ def process_applications():
         .appName("Application Processor") \
         .getOrCreate()
 
-    df = spark.read.csv("applications/applications_dataset_1.csv", header=True, inferSchema=True)
+    dt, df = read_csvs(spark)
 
+    # Transform DF into required format
     df = df.withColumn("first_name", split(col("name"), ' ').getItem(0)) \
         .withColumn("last_name", split(col("name"), ' ').getItem(1)) \
         .withColumn("mobile_no", regexp_extract(col("mobile_no"), "\\d+", 0)) \
@@ -37,6 +63,7 @@ def process_applications():
         .select(col("first_name"), col("last_name"), col("membership_id"), col("date_of_birth"), col("email"),
             col("mobile_no"), col("above_18"))
 
+    # Filter successful and failed applications
     valid_email_regex = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\\.(?:com|net)$"
     # Applications with invalid dates, like 31 Feb, are not successful
     success_df = df.filter(col("date_of_birth").isNotNull()) \
@@ -46,9 +73,10 @@ def process_applications():
         .filter(col("name").isNotNull())
     failure_df = df.subtract(success_df)
 
-    # Use pandas here for simplicity of writing to one csv file
-    success_df.toPandas().to_csv("success/1.csv", header=True)
-    failure_df.toPandas().to_csv("failure/1.csv", header=True)
+    # Write output to different CSV files
+    # Use pandas here for simplicity of writing to one CSV file
+    success_df.toPandas().to_csv(f"success/{dt}.csv", header=True)
+    failure_df.toPandas().to_csv(f"failure/{dt}.csv", header=True)
 
     spark.stop()
 
